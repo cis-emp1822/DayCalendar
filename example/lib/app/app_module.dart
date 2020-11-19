5d1e4dab82c1c78cd8033af7f61f5845c86036f2
Parikshit Pahare <parikshit.p@cisinlabs.com>
2020-11-19 20:14:40 +0530

    also fixed example module

--- example/lib/app/app_module.dart
+++ example/lib/app/app_module.dart
@@ -1,22 +1,22 @@
 import 'package:example/app/app_controller.dart';
 import 'package:example/app/modules/home/home_module.dart';
-import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_modular/flutter_modular.dart' as fm;
 import 'package:flutter/material.dart';
 import 'package:example/app/app_widget.dart';
 
-class AppModule extends MainModule {
+class AppModule extends fm.MainModule {
   @override
-  List<Bind> get binds => [
-        Bind((i) => AppController()),
+  List<fm.Bind> get binds => [
+        fm.Bind((i) => AppController()),
       ];
 
   @override
-  List<Router> get routers => [
-        Router('/', module: HomeModule()),
+  List<fm.Router> get routers => [
+        fm.Router('/', module: HomeModule()),
       ];
 
   @override
   Widget get bootstrap => AppWidget();
 
-  static Inject get to => Inject<AppModule>.of();
+  static fm.Inject get to => fm.Inject<AppModule>.of();
 }
